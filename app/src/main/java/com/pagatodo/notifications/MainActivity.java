e52433e12b6146da7e53c781ff87fdcf4a46144a
roldan <roldan.a.z.p@gmail.com>
2019-07-29 12:01:46 -0500

    Permite testear las notificaciones sin tener que generar la libreria

--- app/src/main/java/com/pagatodo/notifications/MainActivity.java
+++ app/src/main/java/com/pagatodo/notifications/MainActivity.java
@@ -9,5 +9,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        String notificacionApplicationId = "com_pagatodo_apidemo_PE_debug";
+//        String tpvcod = ApiInstance.getInstance().getDatosSesion().getDatosTPV().getTpvcod();
+        String tpvcod = "00001991";
+        com.pagatodo.notifications.AbstractDialogFragment.initIdentity(
+                notificacionApplicationId,
+                tpvcod
+        );
+
+
+        NotificacionesDialogFragment notificacionesDialogFragment = new NotificacionesDialogFragment();
+        notificacionesDialogFragment.setNotificacionIconFragment(new NotificacionIconFragment());
+        notificacionesDialogFragment.show(getFragmentManager(), "");
     }
 }
