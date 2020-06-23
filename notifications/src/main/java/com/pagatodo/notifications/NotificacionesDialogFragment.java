26f4818f4732d8f61fa9e51cde6bc31411a44e05
Jair Jacobo <jair.jacobo@gmail.com>
2020-06-23 14:49:33 -0500

    Se corrige pequeño issue con los tabs

--- notifications/src/main/java/com/pagatodo/notifications/NotificacionesDialogFragment.java
+++ notifications/src/main/java/com/pagatodo/notifications/NotificacionesDialogFragment.java
@@ -14,6 +14,7 @@ import android.view.Window;
 import android.widget.FrameLayout;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
@@ -32,248 +33,246 @@ import java.util.ArrayList;
 
 public class NotificacionesDialogFragment extends AbstractDialogFragment {
 
-	//----------UI-------------------------------------------------------
-	private FragmentLibDialogNotificacionesBinding binding;
-	private NotificacionIconFragment notificacionIconFragment;
-	private int numberNotification=0;
-	private int numberAfiliaciones=0;
-	private ArrayList<Notificacion> listaMensajes;
-	private FirebaseFirestore databasefb = FirebaseFirestore.getInstance();
-	//----- Inst ----------------------------------------------------------
+    //----------UI-------------------------------------------------------
+    private FragmentLibDialogNotificacionesBinding binding;
+    private NotificacionIconFragment notificacionIconFragment;
+    private int numberNotification = 0;
+    private int numberAfiliaciones = 0;
+    private ArrayList<Notificacion> listaMensajes;
+    private FirebaseFirestore databasefb = FirebaseFirestore.getInstance();
+    //----- Inst ----------------------------------------------------------
 
-	//----- Var ----------------------------------------------------------
-	int detailId;
-	private boolean isLandScape;
+    //----- Var ----------------------------------------------------------
+    int detailId;
+    private boolean isLandScape;
 
-	public void loadFragmentLista() {
-		binding.configMenuDetail.setVisibility(isLandScape ? View.GONE : View.VISIBLE);
-	}
+    public void loadFragmentLista() {
+        binding.configMenuDetail.setVisibility(isLandScape ? View.GONE : View.VISIBLE);
+    }
 
-	@Override
-	public void onCreate(final Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		isLandScape = isLandscape();
-	}
+    @Override
+    public void onCreate(final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        isLandScape = isLandscape();
+    }
 
-	@Override
-	public View onCreateView(final LayoutInflater inflater, final ViewGroup container, final Bundle savedInstanceState) {
-		initUI(inflater, container);
+    @Override
+    public View onCreateView(@NonNull final LayoutInflater inflater, final ViewGroup container, final Bundle savedInstanceState) {
+        initUI(inflater, container);
 
-		if (getDialog() != null && getDialog().getWindow() != null) {
-			getDialog().getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
-			getDialog().getWindow().requestFeature(Window.FEATURE_NO_TITLE);
-		}
-		return binding.getRoot();
-	}
+        if (getDialog() != null && getDialog().getWindow() != null) {
+            getDialog().getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+            getDialog().getWindow().requestFeature(Window.FEATURE_NO_TITLE);
+        }
+        return binding.getRoot();
+    }
 
-	@SuppressLint("ClickableViewAccessibility")
-	private void initUI(final LayoutInflater inflater, final ViewGroup container) {
-		binding = DataBindingUtil.inflate(inflater, R.layout.fragment_lib_dialog_notificaciones, container, false);
-		detailId = isLandScape ? R.id.config_menu_detail : R.id.config_menu_items;
-		binding.configMenuDetail.setVisibility(isLandScape ? View.VISIBLE : View.GONE);
+    @SuppressLint("ClickableViewAccessibility")
+    private void initUI(final LayoutInflater inflater, final ViewGroup container) {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_lib_dialog_notificaciones, container, false);
+        detailId = isLandScape ? R.id.config_menu_detail : R.id.config_menu_items;
+        binding.configMenuDetail.setVisibility(isLandScape ? View.VISIBLE : View.GONE);
 
-		PagerAdapter pagerAdapter = new PagerAdapter(getChildFragmentManager());
-		binding.viewPager.setAdapter(pagerAdapter);
-		binding.tablayout.setupWithViewPager(binding.viewPager);
+        PagerAdapter pagerAdapter = new PagerAdapter(getChildFragmentManager());
+        binding.viewPager.setAdapter(pagerAdapter);
+        binding.tablayout.setupWithViewPager(binding.viewPager);
 
-		initMessagesModules();
-		initInboxModules();
+        initMessagesModules();
+        initInboxModules();
 
-		binding.tablayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
-			@Override
-			public void onTabSelected(TabLayout.Tab tab) {
-				switch (tab.getPosition()) {
-					case 0:
-						break;
-					case 1:
-						changeStatusNotification(listaMensajes, getString(
-								R.string.firestore_mensajes,
-								applicationId,
-								tpv));
-						break;
-						default:
-							break;
-				}
-			}
+        binding.tablayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                switch (tab.getPosition()) {
+                    case 0:
+                        break;
+                    case 1:
+                        changeStatusNotification(listaMensajes, getString(
+                                R.string.firestore_mensajes,
+                                applicationId,
+                                tpv));
+                        break;
+                    default:
+                        break;
+                }
+            }
 
-			@Override
-			public void onTabUnselected(TabLayout.Tab tab) {
-				//No implementation
-			}
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+                //No implementation
+            }
 
-			@Override
-			public void onTabReselected(TabLayout.Tab tab) {
-				//No implementation
-			}
-		});
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+                //No implementation
+            }
+        });
 
-		binding.getRoot().setOnTouchListener(new View.OnTouchListener() {
-			@Override
-			public boolean onTouch(final View view, final MotionEvent motionEvent) {
-				PreferenceManager.putNotificationTimestamp(getActivity());
-				return false;
-			}
-		});
+        binding.getRoot().setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(final View view, final MotionEvent motionEvent) {
+                PreferenceManager.putNotificationTimestamp(getActivity());
+                return false;
+            }
+        });
 
-		binding.ivCerrar.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(final View view) {
-				dismiss();
+        binding.ivCerrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(final View view) {
+                dismiss();
 
-			}
-		});
-	}
+            }
+        });
+    }
 
-	private void changeStatusNotification(ArrayList<Notificacion> listaMensajes, String path){
-		DocumentReference notificationReference;
-		for(Notificacion n : listaMensajes) {
-			//Obtener el documento que se va a modificar y cambiar el estatus a leido
-			if(!n.isLeida()) {
-				notificationReference = databasefb.collection(path).document(n.getId());
-				notificationReference.update("leida", true);
-			}
-		}
-	}
+    private void changeStatusNotification(ArrayList<Notificacion> listaMensajes, String path) {
+        DocumentReference notificationReference;
+        for (Notificacion n : listaMensajes) {
+            //Obtener el documento que se va a modificar y cambiar el estatus a leido
+            if (!n.isLeida()) {
+                notificationReference = databasefb.collection(path).document(n.getId());
+                notificationReference.update("leida", true);
+            }
+        }
+    }
 
-	private void loadNumberBadgeInbox(){
-		if(binding.tablayout.getTabAt(0)!=null) {
-			TextView txt2 = binding.tablayout.getTabAt(0).getCustomView().findViewById(R.id.tab_badge);
+    private void loadNumberBadgeInbox() {
+        if (binding.tablayout.getTabAt(0) != null) {
+            TextView txt2 = binding.tablayout.getTabAt(0).getCustomView().findViewById(R.id.tab_badge);
 
-			if (numberNotification > 0) {
-				txt2.setVisibility(View.VISIBLE);
-				txt2.setText(String.valueOf(numberNotification));
-			} else {
-				txt2.setVisibility(View.GONE);
-				txt2.setText(String.valueOf(numberNotification));
-			}
-		}
-	}
+            if (numberNotification > 0) {
+                txt2.setVisibility(View.VISIBLE);
+                txt2.setText(String.valueOf(numberNotification));
+            } else {
+                txt2.setVisibility(View.GONE);
+                txt2.setText(String.valueOf(numberNotification));
+            }
+        }
+    }
 
-	private void loadNumberBadgeMessage(){
-		if(binding.tablayout.getTabAt(1)!=null) {
-			TextView txt4 = binding.tablayout.getTabAt(1).getCustomView().findViewById(R.id.tab_badge);
+    private void loadNumberBadgeMessage() {
+        if (binding.tablayout.getTabAt(1) != null) {
+            TextView txt4 = binding.tablayout.getTabAt(1).getCustomView().findViewById(R.id.tab_badge);
 
-			if (numberAfiliaciones > 0) {
-				txt4.setVisibility(View.VISIBLE);
-				txt4.setText(String.valueOf(numberAfiliaciones));
-			} else {
-				txt4.setVisibility(View.GONE);
-				txt4.setText(String.valueOf(numberAfiliaciones));
-			}
+            if (numberAfiliaciones > 0) {
+                txt4.setVisibility(View.VISIBLE);
+                txt4.setText(String.valueOf(numberAfiliaciones));
+            } else {
+                txt4.setVisibility(View.GONE);
+                txt4.setText(String.valueOf(numberAfiliaciones));
+            }
 
-		}
-	}
+        }
+    }
 
-	private void initInboxModules() {
-		final Query queryInbox = databasefb.collection(getString(
-				R.string.firestore_notificacion,
-				applicationId,
-				tpv));
+    private void initInboxModules() {
+        final Query queryInbox = databasefb.collection(getString(
+                R.string.firestore_notificacion,
+                applicationId,
+                tpv));
 
-		binding.tablayout.getTabAt(0).setCustomView(R.layout.layout_custom_tab_badge);
-		TextView txt1 = binding.tablayout.getTabAt(0).getCustomView().findViewById(R.id.tab_text);
-		txt1.setText("Notificaciones");
+        binding.tablayout.getTabAt(0).setCustomView(R.layout.layout_custom_tab_badge);
+        TextView txt1 = binding.tablayout.getTabAt(0).getCustomView().findViewById(R.id.tab_text);
+        txt1.setText("Notificaciones");
 
-		queryInbox.addSnapshotListener(new EventListener<QuerySnapshot>() {
-			@Override
-			public void onEvent(final @Nullable QuerySnapshot snapshots,
-								final @Nullable FirebaseFirestoreException firestoreException) {
+        queryInbox.addSnapshotListener(new EventListener<QuerySnapshot>() {
+            @Override
+            public void onEvent(final @Nullable QuerySnapshot snapshots,
+                                final @Nullable FirebaseFirestoreException firestoreException) {
+                numberNotification = 0;
 
-				numberNotification = 0;
+                if (snapshots != null && !snapshots.getDocuments().isEmpty()) {
+                    Notificacion notificacion;
 
+                    for (final DocumentSnapshot documentSnapshot : snapshots.getDocuments()) {
+                        notificacion = parseNotificacion(documentSnapshot);
+                        if (!notificacion.isLeida()) {
+                            numberNotification += 1;
+                        }
+                    }
+                    loadNumberBadgeInbox();
+                }
+            }
+        });
 
-					if (snapshots != null && !snapshots.getDocuments().isEmpty()) {
-						Notificacion notificacion;
+    }
 
-						for (final DocumentSnapshot documentSnapshot : snapshots.getDocuments()) {
-							notificacion = parseNotificacion(documentSnapshot);
-							if (!notificacion.isLeida()) {
-								numberNotification += 1;
-							}
-						}
-						loadNumberBadgeInbox();
-					}
-				}
-		});
+    private void initMessagesModules() {
 
-	}
+        final Query queryMensajes = databasefb.collection(getString(
+                R.string.firestore_mensajes,
+                applicationId,
+                tpv));
 
-	private void initMessagesModules() {
+        binding.tablayout.getTabAt(1).setCustomView(R.layout.layout_custom_tab_badge);
+        TextView txt3 = binding.tablayout.getTabAt(1).getCustomView().findViewById(R.id.tab_text);
+        txt3.setText("Comunidad");
 
-		final Query queryMensajes = databasefb.collection(getString(
-				R.string.firestore_mensajes,
-				applicationId,
-				tpv));
+        queryMensajes.addSnapshotListener(new EventListener<QuerySnapshot>() {
+            @Override
+            public void onEvent(final @Nullable QuerySnapshot snapshots,
+                                final @Nullable FirebaseFirestoreException firestoreException) {
 
-		binding.tablayout.getTabAt(1).setCustomView(R.layout.layout_custom_tab_badge);
-		TextView txt3 = binding.tablayout.getTabAt(1).getCustomView().findViewById(R.id.tab_text);
-		txt3.setText("Comunidad");
+                numberAfiliaciones = 0;
+                listaMensajes = null;
+                listaMensajes = new ArrayList<>();
 
-		queryMensajes.addSnapshotListener(new EventListener<QuerySnapshot>() {
-			@Override
-			public void onEvent(final @Nullable QuerySnapshot snapshots,
-								final @Nullable FirebaseFirestoreException firestoreException) {
+                if (snapshots != null && !snapshots.getDocuments().isEmpty()) {
+                    Notificacion notificacion;
 
-				numberAfiliaciones = 0;
-				listaMensajes = null;
-				listaMensajes = new ArrayList<>();
+                    for (final DocumentSnapshot documentSnapshot : snapshots.getDocuments()) {
+                        notificacion = parseNotificacion(documentSnapshot);
+                        if (!notificacion.isLeida()) {
+                            numberAfiliaciones += 1;
+                        }
+                        listaMensajes.add(notificacion);
+                    }
+                    loadNumberBadgeMessage();
+                } else {
+                    binding.tablayout.removeTabAt(1);
+                }
+            }
+        });
+    }
 
-					if (snapshots != null && !snapshots.getDocuments().isEmpty()) {
-						Notificacion notificacion;
+    private Notificacion parseNotificacion(final DocumentSnapshot documentSnapshot) {
+        final Notificacion notificacion = documentSnapshot.toObject(Notificacion.class);
+        notificacion.setId(documentSnapshot.getId());
+        return notificacion;
+    }
 
-						for (final DocumentSnapshot documentSnapshot : snapshots.getDocuments()) {
-							notificacion = parseNotificacion(documentSnapshot);
-							if (!notificacion.isLeida()) {
-								numberAfiliaciones += 1;
-							}
-							listaMensajes.add(notificacion);
-						}
-						loadNumberBadgeMessage();
-					} else {
-						binding.tablayout.removeTabAt(1);
-					}
-				}
-		});
-	}
+    @Override
+    @SuppressLint("ClickableViewAccessibility")
+    public void onStart() {
+        super.onStart();
+        final Dialog dialog = getDialog();
+        if (dialog != null) {
+            int width = ViewGroup.LayoutParams.MATCH_PARENT;
+            int height = ViewGroup.LayoutParams.MATCH_PARENT;
+            dialog.getWindow().setLayout(width, height);
+            final FrameLayout.LayoutParams params = new FrameLayout.LayoutParams(FrameLayout.LayoutParams.MATCH_PARENT, FrameLayout.LayoutParams.MATCH_PARENT);
+            params.gravity = Gravity.CENTER;
+            binding.getRoot().setLayoutParams(params);
+        }
+    }
 
-	private Notificacion parseNotificacion(final DocumentSnapshot documentSnapshot){
-		final Notificacion notificacion = documentSnapshot.toObject(Notificacion.class);
-		notificacion.setId(documentSnapshot.getId());
-		return notificacion;
-	}
+    public void setNotificacionIconFragment(final NotificacionIconFragment notificacionIconFragment) {
+        this.notificacionIconFragment = notificacionIconFragment;
+    }
 
-	@Override
-	@SuppressLint("ClickableViewAccessibility")
-	public void onStart() {
-		super.onStart();
-		final Dialog dialog = getDialog();
-		if (dialog != null) {
-			int width = ViewGroup.LayoutParams.MATCH_PARENT;
-			int height = ViewGroup.LayoutParams.MATCH_PARENT;
-			dialog.getWindow().setLayout(width, height);
-			final FrameLayout.LayoutParams params = new FrameLayout.LayoutParams(FrameLayout.LayoutParams.MATCH_PARENT, FrameLayout.LayoutParams.MATCH_PARENT);
-			params.gravity = Gravity.CENTER;
-			binding.getRoot().setLayoutParams(params);
-		}
-	}
+    public void seleccionaNotificacion(final Notificacion notificacion) {
+        binding.configMenuDetail.setVisibility(View.VISIBLE);
+        cargarFragment(getChildFragmentManager(), NotificacionDetalleFragment.newInstance(notificacion), detailId);
+    }
 
-	public void setNotificacionIconFragment(final NotificacionIconFragment notificacionIconFragment) {
-		this.notificacionIconFragment = notificacionIconFragment;
-	}
-
-	public void seleccionaNotificacion(final Notificacion notificacion) {
-		binding.configMenuDetail.setVisibility(View.VISIBLE);
-		cargarFragment(getChildFragmentManager(), NotificacionDetalleFragment.newInstance(notificacion), detailId);
-	}
-
-	public void actualizarDetalleNotificacion(final Notificacion notificacion) {
-		if (binding.configMenuDetail.getVisibility() == View.VISIBLE) {
-			final Fragment fragment = getChildFragmentManager().findFragmentByTag(NotificacionDetalleFragment.class.getSimpleName());
-			if (fragment != null
-					&& fragment.isVisible()
-					&& ((NotificacionDetalleFragment) fragment).getNotificationId().equals(notificacion.getId())) {
-				cargarFragment(getChildFragmentManager(), NotificacionDetalleFragment.newInstance(notificacion), detailId);
-			}
-		}
-	}
+    public void actualizarDetalleNotificacion(final Notificacion notificacion) {
+        if (binding.configMenuDetail.getVisibility() == View.VISIBLE) {
+            final Fragment fragment = getChildFragmentManager().findFragmentByTag(NotificacionDetalleFragment.class.getSimpleName());
+            if (fragment != null
+                    && fragment.isVisible()
+                    && ((NotificacionDetalleFragment) fragment).getNotificationId().equals(notificacion.getId())) {
+                cargarFragment(getChildFragmentManager(), NotificacionDetalleFragment.newInstance(notificacion), detailId);
+            }
+        }
+    }
 }
